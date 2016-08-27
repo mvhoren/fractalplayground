3f1e26171b474bbc2595d24a1bced818de77d5fd
Joakim <->
2016-08-27 21:07:34 +0200

    Updated test

--- FractalTests/RangeTests.cs
+++ FractalTests/RangeTests.cs
@@ -8,11 +8,11 @@ namespace FractalTests
 	{
 		[TestMethod]
 		public void can_calculate_span() =>
-			Assert.IsTrue(new Range(1, 3).Span == 2);
+			Assert.AreEqual(new Range(1, 3).Span, 2);
 
 		[TestMethod]
 		public void can_express_value_as_unit() =>
-			Assert.IsTrue(new Range(0, 10).AsUnit(5) == 0.5);
+			Assert.AreEqual(new Range(0, 10).AsUnit(5), 0.5);
 
 		[TestMethod]
 		public void can_map_value_to_range()
@@ -21,13 +21,13 @@ namespace FractalTests
 			var toRange = new Range(1, 10);
 
 			var x = fromRange.Map(fromRange.From, toRange);
-			Assert.IsTrue(x == toRange.From);
+			Assert.AreEqual(x, toRange.From);
 
 			var y = fromRange.Map(fromRange.To, toRange);
-			Assert.IsTrue(y == toRange.To);
+			Assert.AreEqual(y, toRange.To);
 
 			var z = fromRange.Map(fromRange.To - fromRange.From, toRange);
-			Assert.IsTrue(z == toRange.To);
+			Assert.AreEqual(z, toRange.To);
 		}
 	}
 }
